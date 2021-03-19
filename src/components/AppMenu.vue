2dc264f6850c184e414df4c342b94c31a6f3ea1f
ni4yja <k2.liuba@gmail.com>
2021-03-19 14:16:16 +0200

    burger menu fix

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <span class="material-icons burger" @click="toggleActive">menu</span>
+  <div class="app-menu-tab">
+    <span class="material-icons burger" @click="toggleActive" ref="button">menu</span>
     <section class="app-menu" :class="[showNav ? 'slide-in' : 'slide-out']">
-      <div class="app-menu-wrapper" v-show="menuActive">
+      <div class="app-menu-wrapper" 
+        v-show="menuActive"  
+        v-closable="{
+          exclude: ['button'],
+          handler: 'onClose'
+        }"
+      >
         <router-link class="title is-5" to="/">Photo App Logo</router-link>
         <nav class="nav">
           <router-link class="subtitle is-5" :to="{ name: 'home' }">Home</router-link>
@@ -17,7 +23,57 @@
 </template>
 
 <script>
+import Vue from 'vue'
 
+// This variable will hold the reference to
+// document's click handler
+let handleOutsideClick
+
+Vue.directive('closable', {
+  bind (el, binding, vnode) {
+    // Here's the click/touchstart handler
+    // (it is registered below)
+    handleOutsideClick = (e) => {
+      e.stopPropagation()
+      // Get the handler method name and the exclude array
+      // from the object used in v-closable
+      const { handler, exclude } = binding.value
+
+      // This variable indicates if the clicked element is excluded
+      let clickedOnExcludedEl = false
+      exclude.forEach(refName => {
+        // We only run this code if we haven't detected
+        // any excluded element yet
+        if (!clickedOnExcludedEl) {
+          // Get the element using the reference name
+          const excludedEl = vnode.context.$refs[refName]
+          // See if this excluded element
+          // is the same element the user just clicked on
+          clickedOnExcludedEl = excludedEl.contains(e.target)
+        }
+      })
+
+      // We check to see if the clicked element is not
+      // the dialog element and not excluded
+      if (!el.contains(e.target) && !clickedOnExcludedEl) {
+        // If the clicked element is outside the dialog
+        // and not the button, then call the outside-click handler
+        // from the same component this directive is used in
+        vnode.context[handler]()
+      }
+    }
+    // Register click/touchstart event listeners on the whole page
+    document.addEventListener('click', handleOutsideClick)
+    document.addEventListener('touchstart', handleOutsideClick)
+  },
+
+  unbind () {
+    // If the element that has v-closable is removed, then
+    // unbind click/touchstart listeners from the whole page
+    document.removeEventListener('click', handleOutsideClick)
+    document.removeEventListener('touchstart', handleOutsideClick)
+  }
+})
 export default {
   data() {
     return {
@@ -31,7 +87,10 @@ export default {
       this.showNav = !this.showNav
     },
     onClose() {
-      this.showNav = false
+      if (window.innerWidth <= 768) {
+        this.showNav = false
+        this.menuActive = false
+      }
     }
   }
 }
